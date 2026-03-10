4e69af9ee9ab973862f65ac8fa6886e738dea5da
Prisha Chapla <prishachapla2007@gmail.com>
2026-03-10 09:01:14 +0530

    UC5: Inline array initialization using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,27 +1,17 @@
-/**
- * OOPSBannerApp - UC4
- * Prints "OOPS" banner using String array and loop
- * for better modularity and reusability.
- *
- * @author Prisha Chapla
- * @version 4.0
- */
-
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
         String[] banner = {
-                String.join("   ", " ***** ", " ***** ", " ***** ", " ***** "),
-                String.join("   ", "*     *", "*     *", "*     *", "*     *"),
-                String.join("   ", "*     *", "*     *", "*     *", "*     *"),
-                String.join("   ", "*     *", "*     *", "*     *", "*     *"),
-                String.join("   ", "*     *", "*     *", "*     *", "*     *"),
-                String.join("   ", "*     *", "*     *", "*     *", "*     *"),
-                String.join("   ", " ***** ", " ***** ", " ***** ", " ***** ")
+            String.join("   ", " ***** ", " ***** ", " ***** ", " ***** "),
+            String.join("   ", "*     *", "*     *", "*     *", "*     *"),
+            String.join("   ", "*     *", "*     *", "*     *", "*     *"),
+            String.join("   ", "*     *", "*     *", "*     *", "*     *"),
+            String.join("   ", "*     *", "*     *", "*     *", "*     *"),
+            String.join("   ", "*     *", "*     *", "*     *", "*     *"),
+            String.join("   ", " ***** ", " ***** ", " ***** ", " ***** ")
         };
 
-        // Enhanced for-loop
         for (String line : banner) {
             System.out.println(line);
         }
